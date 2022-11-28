3be7c681f7b06f3ff5da70659c5ead186836cf8b
iain <iain@rootisgod.com>
2022-11-28 19:36:31 +0000

    Testing env creation and lookup

--- Local/map-testing/modules/file-creator/main.tf
+++ Local/map-testing/modules/file-creator/main.tf
@@ -1,1 +1,4 @@
 #
+locals {
+  test =
+}
---
+++ OctopusDeploy/LookupEnvironments/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    octopusdeploy = {
+      source  = "OctopusDeployLabs/octopusdeploy"
+    }
+  }
+}
+
+provider "octopusdeploy" {
+  address  = var.serverURL
+  api_key  = var.apiKey
+  space_id = var.space
+}
+
+data "octopusdeploy_environments" "this" {
+  name = upper("dev")
+#  partial_name = "PR*"
+  skip = 0
+  take = 1
+}
+
+output "envs_found" {
+  value = data.octopusdeploy_environments.this.*
+}
+
+output "env_name" {
+  value = data.octopusdeploy_environments.this.name
+}
