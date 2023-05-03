2ea8ca20458e63caaf5398d42be01469d033d2bf
Iain Black <iain@rootisgod.com>
2023-05-03 17:37:57 +0100

    Working dynamic block add

--- AzureRM/DynamicNSGModule/modules/nsg/main.tf
+++ AzureRM/DynamicNSGModule/modules/nsg/main.tf
@@ -10,6 +10,7 @@ resource "azurerm_network_security_group" "this" {
   resource_group_name = var.rg
 
 # Need to make dynamic based on the value of another var
+# https://stackoverflow.com/questions/63592602/optional-block-based-on-variable
 # rulename = "skip" or "create"
 
 #  security_rule {
@@ -23,4 +24,22 @@ resource "azurerm_network_security_group" "this" {
 #    source_address_prefix      = "*"
 #    destination_address_prefix = "*"
 #  }
+
+  dynamic "security_rule" {
+
+    for_each = var.priority == 101 ? toset([]) : toset([1])
+
+    content {
+      name                       = var.name
+      priority                   = var.priority
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "*"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  }
+
 }
